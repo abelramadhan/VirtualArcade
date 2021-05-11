9f48aabe5fa35edfab60f352c4f4a31ce9bc8d8a
Kautsarratu <64664923+Kautsarratu@users.noreply.github.com>
2021-05-12 00:17:07 +0700

    Revert "penambahan sudoku"
    This reverts commit b3542b1f1483e8ea278a7edb3c45e1a3e8f05a93.

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,3 @@ Route::get('/login', function () {
 Route::get('/register', function () {
     return view('authentication.register');
 });
-
-Route::get('/sudoku', function () {
-    return view('sudoku.playsudoku');
-});
